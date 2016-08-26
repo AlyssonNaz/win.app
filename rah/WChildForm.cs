f46f781e4a16e91bf702f0c38ee875c858293a48
sysworldti <ricardo@sysworldti.com.br>
2016-08-25 23:28:45 -0300

    Construção #1031

---
+++ rah/WChildForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace rah
+{
+    public partial class WChildForm : rah.lib.core.WForm
+    {
+        public WChildForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
