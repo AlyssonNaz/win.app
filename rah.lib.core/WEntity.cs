edcd0abfd4ab89d1539ff5e0304cadf6902ca55e
sysworldti <ricardo@sysworldti.com.br>
2016-08-27 09:05:09 -0300

    criação do entity/list

--- rah.lib.core/WEntity.cs
+++ rah.lib.core/WEntity.cs
@@ -1,11 +1,18 @@
-﻿namespace rah.lib.core
+﻿using System;
+
+namespace rah.lib.core
 {
     public interface IWEntity
     {
-
+        int Id { set; get; }
+        DateTime DataCriacao { set; get; }
+        DateTime DataModificacao { set; get; }
     }
 
     public class WEntity : IWEntity
     {
+        public int Id { set; get; }
+        public DateTime DataCriacao { set; get; }
+        public DateTime DataModificacao { set; get; }
     }
 }
--- rah.lib.core/WList.cs
+++ rah.lib.core/WList.cs
@@ -2,10 +2,17 @@
 {
     public interface IWList
     {
-
+        int Count { get; set; }
+        int Page { get; set; }
+        int Size { get; set; }
+        object Itens { get; set; }
     }
 
     public class WList : IWList
     {
+        public int Count { get; set; }
+        public int Page { get; set; }
+        public int Size { get; set; }
+        public object Itens { get; set; }
     }
 }
