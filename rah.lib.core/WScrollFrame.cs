46ac21c31ab36b1441dbeb13c73c6a77c32d4049
sysworldti <ricardo@sysworldti.com.br>
2016-08-28 14:51:22 -0300

    padrão composite

--- rah.lib.core/WScrollFrame.cs
+++ rah.lib.core/WScrollFrame.cs
@@ -1,4 +1,6 @@
-﻿namespace rah.lib.core
+﻿using System;
+
+namespace rah.lib.core
 {
     public partial class WScrollFrame : WFrame
     {
@@ -7,36 +9,61 @@
             InitializeComponent();
         }
 
-        public WScrollFrame(IWEntity Entity)
-        {
-            InitializeComponent();
-            _entity = Entity;
-        }
-
-        private IWEntity _entity = null;
         private WScrollFrame _compositeTopFrame = null;
         private WScrollFrame _compositeLeftFrame = null;
         private WScrollFrame _compositeRightFrame = null;
         private WScrollFrame _compositeBottomFrame = null;
 
-        protected void CompositeTopFrame()
+        protected void CompositeTopFrame(Type type)
         {
-            _compositeBottomFrame._entity = _entity;
+            if (_compositeTopFrame != null)
+                throw new Exception($"{type.Name} já foi registrada.");
+
+            _compositeTopFrame = (WScrollFrame)Activator.CreateInstance(type);
+            _compositeTopFrame.Parent = panRight;
+            _compositeTopFrame.Dock = System.Windows.Forms.DockStyle.Fill;
+
+            panTop.Height = _compositeTopFrame.Height;
+            panTop.Visible = true;
         }
 
-        protected void CompositeLeftFrame()
+        protected void CompositeLeftFrame(Type type)
         {
+            if (_compositeLeftFrame != null)
+                throw new Exception($"{type.Name} já foi registrada.");
 
-        }        
+            _compositeLeftFrame = (WScrollFrame)Activator.CreateInstance(type);
+            _compositeLeftFrame.Parent = panRight;
+            _compositeLeftFrame.Dock = System.Windows.Forms.DockStyle.Fill;
 
-        protected void CompositeRightFrame()
-        {
-
+            panLeft.Width = _compositeLeftFrame.Width;
+            panLeft.Visible = true;
         }
 
-        protected void CompositeBottomFrame()
+        protected void CompositeRightFrame(Type type)
         {
+            if(_compositeRightFrame != null)
+                throw new Exception($"{type.Name} já foi registrada.");
 
+            _compositeRightFrame = (WScrollFrame)Activator.CreateInstance(type);
+            _compositeRightFrame.Parent = panRight;
+            _compositeRightFrame.Dock = System.Windows.Forms.DockStyle.Fill;
+
+            panRight.Width = _compositeRightFrame.Width;
+            panRight.Visible = true;
+        }
+
+        protected void CompositeBottomFrame(Type type)
+        {
+            if (_compositeBottomFrame != null)
+                throw new Exception($"{type.Name} já foi registrada.");
+
+            _compositeBottomFrame = (WScrollFrame)Activator.CreateInstance(type);
+            _compositeBottomFrame.Parent = panBottom;
+            _compositeBottomFrame.Dock = System.Windows.Forms.DockStyle.Fill;
+
+            panBottom.Height = _compositeBottomFrame.Height;
+            panBottom.Visible = true;
         }
     }
 }
