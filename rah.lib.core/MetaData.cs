98275d31a802743633662206fa2d7ede33c86cfb
sysworldti <ricardo@sysworldti.com.br>
2016-08-28 23:12:41 -0300

    metadata

---
+++ rah.lib.core/MetaData.cs
@@ -0,0 +1,16 @@
+﻿namespace rah.lib.core
+{
+    public class MetaData
+    {
+        public string Name { get; set; }
+        public string Caption { get; set; }
+        public string DataType { get; set; }
+        public int Size { get; set; }
+        public bool Required { get; set; }
+        public bool ReadOnly { get; set; }
+
+        public void Load(string metaData)
+        {
+        }
+    }
+}
