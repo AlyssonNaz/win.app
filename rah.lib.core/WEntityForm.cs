4231e9ef0ceaf79559eaab21ca7ba6c645a7a055
sysworldti <ricardo@sysworldti.com.br>
2016-08-28 23:25:25 -0300

    entityForm / MetaData

---
+++ rah.lib.core/WEntityForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace rah.lib.core
+{
+    public partial class WEntityForm : WChildForm
+    {
+        private MetaData metadata;
+
+        public WEntityForm(MetaData metadata)
+        {
+            InitializeComponent();
+            this.metadata = metadata;
+            Load();
+        }
+
+        private void Load()
+        {
+        }
+    }
+}
